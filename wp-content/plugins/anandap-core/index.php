ee0751089133b75434b76960a5ec554aa707b0e3
w3dev87 <polarisdev87@outlook.com>
2018-10-16 05:17:13 -0400

    add anandap-core plugin for core features

---
+++ wp-content/plugins/anandap-core/index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: AnandaProfessional Core
+Description: Important functionalities of the AnandaProfessional WordPress site
+Author: Lancelot Gordon
+Version: 0.1
+*/
+
+define("ANANDAP_CORE_PLUGIN_PATH", dirname(__FILE__));
+define("ANANDAP_CORE_PLUGIN_URL", plugins_url('', __FILE__));
+
+require_once "functions.php";
